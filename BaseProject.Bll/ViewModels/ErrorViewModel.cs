4c7b2654f935d0ba985adac590b64ff08bedeab3
Aaron Isaac M <aaron.isaacm@gmail.com>
2018-05-17 21:12:24 -0700

    Initial Project

---
+++ BaseProject.Bll/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BaseProject.Bll.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
